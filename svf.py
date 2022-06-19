d4ffc6934213a8818e6892f2edb4dcfc90069d16
Kyle505 <38930095+Kyle505@users.noreply.github.com>
2022-06-19 16:43:36 +0800

    update svf

--- svf.py
+++ svf.py
@@ -4,17 +4,64 @@ from ctypes import create_string_buffer
 # load the library
 
 
-lib = cdll.LoadLibrary('./lib/libsvf.so')
+lib = cdll.LoadLibrary('./src/libsvf.so')
 
+## Test
 class SVF(object):
   
     # constructor
     def __init__(self, module_name):
         # attribute
-        self.obj = lib.GetSVFModule(module_name)
+        self.obj = lib.initSVFModule(module_name)
+
+    def buildSymbolTableInfo(self):
+        lib.buildSymbolTableInfo()
     
-  
+    def buildSVFIR(self):
+        lib.buildSVFIR()
+
+    def createAndersenWaveDiff(self):
+        lib.createAndersenWaveDiff()
+
+    def getICFG(self):
+        lib.getICFG()
+
+    def getVFG(self):
+        lib.getVFG()
+
+    def buildFullSVFG(self):
+        lib.buildFullSVFG()
+
+    def release(self):
+        lib.release()
+
+
+class SVFModule(object):
+
+    # constructor
+    def __init__(self, module_name):
+        # attribute
+        self.obj = lib.initSVFModule(module_name)
+
+
+    def buildSymbolTableInfo(self):
+        lib.buildSymbolTableInfo()
+
 
 module_name = create_string_buffer(b"example.ll")
-f = SVF(module_name)
+
+# Init
+svf = SVF(module_name)
+svf.buildSymbolTableInfo()
+
+svf.buildSVFIR()
+
+svf.createAndersenWaveDiff()
+
+svf.getICFG()
+svf.getVFG()
+
+svf.buildFullSVFG()
+
+svf.release()
 
